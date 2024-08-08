8f47122c026811c0030cdaa955a2f29bd624eecb
vmgn16 <mg_619@hotmail.com>
2024-08-08 01:35:30 -0600

    agregada la funcionalidad get_user_by_username

--- db/db_user.py
+++ db/db_user.py
@@ -25,6 +25,13 @@ def get_user(db: Session, user_id: int):
                             detail=f"User whith id {user_id} not found")
     return user
 
+def get_user_by_username(db: Session, username: str):
+    user = db.query(DbUser).filter(DbUser.username == username).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"User whith username {username} not found")
+    return user
+
 def update_user(db: Session, user_id: int, request: UserBase):
     user = db.query(DbUser).filter(DbUser.id == user_id)
     if not user:
