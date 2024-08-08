23ea6cd6493b02dca160993caa9e930938b81881
vmgn16 <mg_619@hotmail.com>
2024-08-08 01:24:58 -0600

    se agrega authentication router

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from router import (blog_get, blog_post,
-                    user, article, product)
+from router import (blog_get, blog_post, user,
+                    article, product)
+from auth import authentication
 from db import models
 from db.database import engine
 from exceptions import StoryException
@@ -9,6 +10,7 @@ from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.include_router(authentication.router)
 app.include_router(user.router)
 app.include_router(article.router)
 app.include_router(product.router)
