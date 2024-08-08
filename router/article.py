18d6e2cb7b20918c4b403f7f461bf724881fb42f
vmgn16 <mg_619@hotmail.com>
2024-08-08 01:38:36 -0600

    endpoints asegurados

--- router/article.py
+++ router/article.py
@@ -3,7 +3,8 @@ from schemas import ArticleBase, ArticleDisplay
 from sqlalchemy.orm import Session
 from db.database import get_db
 from db import db_article
-from typing import List
+from auth.oauth2 import get_current_user
+from schemas import UserBase
 
 router = APIRouter(
     prefix="/article",
@@ -13,11 +14,11 @@ router = APIRouter(
 
 # Create article
 @router.post("/", response_model=ArticleDisplay)
-def create_article(request: ArticleBase, db: Session = Depends(get_db)):
+def create_article(request: ArticleBase, db: Session = Depends(get_db), current_user: UserBase = Depends(get_current_user)):
     return db_article.create_article(db, request)
 
 # Read specific article
 @router.get("/{id}", response_model=ArticleDisplay)
-def get_article(id: int, db: Session = Depends(get_db)):
+def get_article(id: int, db: Session = Depends(get_db), current_user: UserBase = Depends(get_current_user)):
     return db_article.get_article(db, id)
 
--- router/user.py
+++ router/user.py
@@ -4,6 +4,7 @@ from schemas import UserBase, UserDisplay
 from sqlalchemy.orm import Session
 from db.database import get_db
 from db import db_user
+from auth.oauth2 import get_current_user
 
 router = APIRouter(
     prefix="/user",
@@ -18,20 +19,24 @@ def create_user(request: UserBase, db: Session = Depends(get_db)):
 
 # Read all user
 @router.get("/", response_model=List[UserDisplay])
-def get_all_users(db: Session = Depends(get_db)):
+def get_all_users(db: Session = Depends(get_db), 
+                  current_user: UserBase = Depends(get_current_user)):
     return db_user.get_all_users(db)
 
 # Read single user
 @router.get("/{id}", response_model=UserDisplay)
-def get_user(id: int, db: Session = Depends(get_db)):
+def get_user(id: int, db: Session = Depends(get_db), 
+                  current_user: UserBase = Depends(get_current_user)):
     return db_user.get_user(db, id)
 
 # Update user
 @router.put("/{id}/update", response_model=UserDisplay)
-def update_user(id: int, request: UserBase, db: Session = Depends(get_db)):
+def update_user(id: int, request: UserBase, db: Session = Depends(get_db), 
+                  current_user: UserBase = Depends(get_current_user)):
     return db_user.update_user(db, id, request)
 
 # Delete user
 @router.delete("/{id}/delete")
-def delete_user(id: int, db: Session = Depends(get_db)):
+def delete_user(id: int, db: Session = Depends(get_db), 
+                  current_user: UserBase = Depends(get_current_user)):
     return db_user.delete_user(db, id)
