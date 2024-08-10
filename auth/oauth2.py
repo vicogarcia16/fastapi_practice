5941c857d88988a161bc738431e46ec966b3b0fc
vmgn16 <mg_619@hotmail.com>
2024-08-09 18:27:33 -0600

    comentado lineas

--- auth/oauth2.py
+++ auth/oauth2.py
@@ -7,10 +7,10 @@ from db.database import get_db
 from sqlalchemy.orm import Session
 from db import db_user
 from fastapi import Depends, HTTPException, status
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 import os
  
-load_dotenv() 
+#load_dotenv() 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
  
 SECRET_KEY = os.getenv('SECRET_KEY')
