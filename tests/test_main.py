ceb74050e018fe43ed7ffaec03037fce65423fb7
vmgn16 <mg_619@hotmail.com>
2024-08-12 18:32:42 -0600

    se agrega pytest y requests

---
+++ tests/test_main.py
@@ -0,0 +1,42 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_get_all_blogs():
+    response = client.get("/blog/all")
+    assert response.status_code == 200
+
+def test_auth_error():
+    response = client.post("/token",
+                data={"username": "","password": ""})
+    access_token = response.json().get('access_token')
+    assert access_token == None
+    message = response.json().get('detail')[0].get('msg')
+    assert message == "Field required"
+    
+def test_auth_success():
+    response = client.post("/token",
+                data={"username": "vico","password": "vico"})
+    access_token = response.json().get('access_token')
+    assert access_token
+    
+def test_post_article():
+    auth = client.post("/token",
+                data={"username": "vico","password": "vico"})
+    access_token = auth.json().get('access_token')
+    assert access_token
+    response = client.post(
+        "/article/",
+        json=(
+            {
+                "title": "Test title",
+                "content": "Test description",
+                "published": True,
+                "creator_id": 2
+            }
+        ),
+        headers={"Authorization": f"Bearer {access_token}"}
+    )
+    assert response.status_code == 200
+    assert response.json().get("title") == "Test title"
