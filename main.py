8ce532b6289377d1ce5378a3997efc47b892c4aa
vmgn16 <mg_619@hotmail.com>
2024-08-14 19:48:45 -0600

    agregado websockets y tarea segundo plano

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from router import (blog_get, blog_post, user,
                     article, product, file)
 from templates import templates
@@ -6,11 +6,12 @@ from auth import authentication
 from db import models
 from db.database import engine
 from tools.exceptions import StoryException
+from tools.client import html
 from fastapi import Request
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-import os
+import os, time
 
 app = FastAPI(
     title="Practice API",
@@ -28,11 +29,43 @@ app.include_router(product.router)
 app.include_router(blog_get.router)
 app.include_router(blog_post.router)
 
+@app.get("/chat/", include_in_schema=False)
+async def chat():
+    return HTMLResponse(html)
+
+clients = []
+messages = []
+
+@app.websocket("/chat/")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    clients.append(websocket)
+    for message in messages:
+        await websocket.send_text(message)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            messages.append(data)
+            for client in clients:
+                await client.send_text(data)
+    except WebSocketDisconnect:
+        clients.remove(websocket)
+      
+    
+
 @app.exception_handler(StoryException)
 def story_exception_handler(request: Request, exc: StoryException):
     return JSONResponse(status_code=418, content={"detail": exc.name})
 
 models.Base.metadata.create_all(bind=engine)
+
+@app.middleware("http")
+async def add_middleware(request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    duration = time.time() - start_time
+    response.headers["duration"] = str(duration)
+    return response
 
 origins = ["http://localhost:3000"]
 app.add_middleware(
--- templates/templates.py
+++ templates/templates.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, BackgroundTasks
 from schemas.schemas import ProductBase
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.requests import Request
+from tools.custom_log import log
 
 router = APIRouter(
     prefix="/templates",
@@ -13,10 +14,17 @@ router = APIRouter(
 templates = Jinja2Templates(directory="templates")
 
 @router.post("/products/{id}/", response_class=HTMLResponse)
-def get_product(id: int, product: ProductBase, request: Request):
+def get_product(id: int, 
+                product: ProductBase, 
+                request: Request,
+                bt: BackgroundTasks):
+    bt.add_task(log_template_call, f"Template read for product with id: {id}")
     return templates.TemplateResponse("product.html", 
                                       {"request": request, 
                                        "id": id,
                                        "title": product.title,
                                        "description": product.description,
                                        "price": product.price})
+
+def log_template_call(message: str):
+    log("MyAPI", message)
