abe84d7274a63c79264d3afd16c5cc7814bb723d
vmgn16 <mg_619@hotmail.com>
2025-01-06 16:27:22 -0600

    cambio en respuesta

--- db/db_user.py
+++ db/db_user.py
@@ -29,7 +29,7 @@ def get_user_by_username(db: Session, username: str):
     user = db.query(DbUser).filter(DbUser.username == username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"User whith username {username} not found")
+                            detail=f"User with username {username} not found")
     return user
 
 def update_user(db: Session, user_id: int, request: UserBase):
