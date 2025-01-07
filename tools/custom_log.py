221048b24847927274dbf4804b22015f8810e730
vmgn16 <mg_619@hotmail.com>
2025-01-06 18:41:01 -0600

    add validation request from custom_log

--- tools/custom_log.py
+++ tools/custom_log.py
@@ -3,4 +3,7 @@ from fastapi.requests import Request
 def log(tag="MyAPI", message="", request: Request = None):
     with open("tools/log.txt", "a+") as log:
         log.write(f"{tag}: {message}\n")
-        log.write(f"\t{request.url}\n")
+        if request:
+            log.write(f"\t{request.url}\n")
+        else:
+            log.write("\tNo request available\n")
